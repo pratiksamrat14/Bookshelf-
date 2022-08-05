4f1e2e14438820d94fcec05da40b50deb7cd02dd
Samrat Pratik Tukaram <pratiksamrat14@gmail.com>
2022-08-05 20:54:55 +0530

    hil

---
+++ client/src/components/Home/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Home_container from '../../containers/home_container';
+const Home = () => {
+    return (
+        <div>
+          <Home_container/>
+        </div>
+    );
+};
+
+export default Home;
